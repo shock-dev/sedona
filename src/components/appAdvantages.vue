780f15c539431a44fd9e9613f8aaadd3f559d9ce
Andrey <shock.work@mail.ru>
2021-01-03 22:34:31 +0300

    added data for this components

--- src/components/appAdvantages.vue
+++ src/components/appAdvantages.vue
@@ -5,21 +5,45 @@
       <p class="advantages__top-text">Рассмотрим 5 причин, по которым Седона круче, чем гранд каньон!</p>
     </div>
 
-    <div class="advantages__items left image">
-      <div class="advantages__item">
-        <div class="advantages__item-title">Настоящий городок</div>
-        <div class="advantages__item-num">— №1 —</div>
-        <div class="advantages__item-text">Седона — не аттракцион для туристов, там течёт своя жизнь</div>
-      </div>
-      <div class="advantages__img">
-        <img src="../assets/img/advantages-bg-1.jpg" alt="">
-      </div>
-    </div>
+    <app-advantages-image-items
+      :key="imageItems[0].title"
+      :title="imageItems[0].title"
+      :description="imageItems[0].description"
+      :number="imageItems[0].number"
+      :imageUrl="imageItems[0].imageUrl"
+    />
+
+    <app-advantages-image-items
+        :key="imageItems[1].title"
+        :title="imageItems[1].title"
+        :description="imageItems[1].description"
+        :number="imageItems[1].number"
+        :imageUrl="imageItems[1].imageUrl"
+        :position="1"
+    />
   </div>
 </template>
 
 <script>
+import AppAdvantagesImageItems from "@/components/appAdvantagesImageItems";
 export default {
-  name: "appAdvantages"
+  name: "appAdvantages",
+  components: {AppAdvantagesImageItems},
+  data: () => ({
+    imageItems: [
+      {
+        title: 'Настоящий городок',
+        description: 'Седона — не аттракцион для туристов, там течёт своя жизнь',
+        number: 1,
+        imageUrl: require('@/assets/img/advantages-bg-1.jpg')
+      },
+      {
+        title: 'Там есть Мост дьявола',
+        description: 'Да, по нему можно пройти! Если конечно вы осмелитесь',
+        number: 2,
+        imageUrl: require('@/assets/img/advantages-bg-2.jpg')
+      }
+    ]
+  })
 }
 </script>
