6028dea7899f24a19250f38dace1c1604a06134c
Andrey <shock.work@mail.ru>
2021-01-03 21:07:18 +0300

    template for first part of advantage items

--- src/components/appAdvantages.vue
+++ src/components/appAdvantages.vue
@@ -4,6 +4,17 @@
       <h3 class="advantages__top-title">Седона — небольшой городок в Аризоне, заслуживающий большего!</h3>
       <p class="advantages__top-text">Рассмотрим 5 причин, по которым Седона круче, чем гранд каньон!</p>
     </div>
+
+    <div class="advantages__items left image">
+      <div class="advantages__item">
+        <div class="advantages__item-title">Настоящий городок</div>
+        <div class="advantages__item-num">— №1 —</div>
+        <div class="advantages__item-text">Седона — не аттракцион для туристов, там течёт своя жизнь</div>
+      </div>
+      <div class="advantages__img">
+        <img src="../assets/img/advantages-bg-1.jpg" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
