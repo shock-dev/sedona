3f16cbee803ec85d4d3487d5cea7fd52972b76d5
Andrey <shock.work@mail.ru>
2021-01-04 02:04:06 +0300

    the ability of the jet to change the data from a child

--- src/components/appFormTextInput.vue
+++ src/components/appFormTextInput.vue
@@ -1,12 +1,23 @@
 <template>
-  <label class="form__label">
-    <span class="form__text">Дата заезда</span>
-    <input class="form__input" type="text">
-  </label>
+  <div>
+    <label class="form__label">
+      <span class="form__text">{{ title }}</span>
+      <input class="form__input" type="text" @input="sendValue" :value="value">
+    </label>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "appFormTextInput"
+  name: "appFormTextInput",
+  props: {
+    title: String,
+    value: String
+  },
+  methods: {
+    sendValue(e) {
+      this.$emit('sendValueToParent', e.target.value)
+    }
+  }
 }
 </script>
