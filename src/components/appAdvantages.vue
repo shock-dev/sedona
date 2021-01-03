692f5a67c152db0f5b11d6441f628b3a2ec82e64
Andrey <shock.work@mail.ru>
2021-01-04 00:45:49 +0300

    integration of the old component

--- src/components/appAdvantages.vue
+++ src/components/appAdvantages.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="advantages">
-    <div class="advantages__top">
-      <h3 class="advantages__top-title">Седона — небольшой городок в Аризоне, заслуживающий большего!</h3>
-      <p class="advantages__top-text">Рассмотрим 5 причин, по которым Седона круче, чем гранд каньон!</p>
-    </div>
+    <app-top-content
+      :title="topContents[0].title"
+      :description="topContents[0].description"
+    />
 
     <app-advantages-image-items
       :key="imageItems[0].title"
