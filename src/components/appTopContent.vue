53f0361e8f8989ea0e08436ea8e51ff4f78dcca4
Andrey <shock.work@mail.ru>
2021-01-04 00:30:11 +0300

    new component ~ top content

---
+++ src/components/appTopContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="top-content">
+    <h3 class="top-content-title">{{ title }}</h3>
+    <p class="top-content-text">{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "appTopContent",
+  props: {
+    title: String,
+    description: String
+  }
+}
+</script>
