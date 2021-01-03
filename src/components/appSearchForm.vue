253544734b1fc6e1281ae08140e4f35262e2c3f1
Andrey <shock.work@mail.ru>
2021-01-04 01:20:50 +0300

    added a map behind the form

--- src/components/appSearchForm.vue
+++ src/components/appSearchForm.vue
@@ -3,7 +3,11 @@
     <div class="form__header">
       Поиск гостиницы в седоне
     </div>
+    <div class="form__map">
+      <div class="form__body">
 
+      </div>
+    </div>
   </form>
 </template>
 
