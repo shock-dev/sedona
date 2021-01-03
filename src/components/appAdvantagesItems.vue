19c7404aa6b8856a91549bf5cbf2a2d883fa8692
Andrey <shock.work@mail.ru>
2021-01-04 00:47:02 +0300

    added items component

---
+++ src/components/appAdvantagesItems.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="advantages__items material">
+    <li
+        v-for="({ imageUrl, title, description, number }, index) in items"
+        :key="index"
+        class="advantages__item"
+        :class="bgc"
+    >
+      <div class="advantages__item-img" v-if="imageUrl">
+        <img :src="imageUrl" alt="">
+      </div>
+      <div class="advantages__item-title">{{ title }}</div>
+      <div class="advantages__item-num" v-if="number">— №{{ number }} —</div>
+      <div class="advantages__item-description">{{ description }}</div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "appAdvantagesItems",
+  props: {
+    items: Array,
+    bgc: {
+      type: String,
+      default: 'white'
+    }
+  }
+}
+</script>
