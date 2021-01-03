c26b628cbd11145b786e7db032ac8110a4cae210
Andrey <shock.work@mail.ru>
2021-01-04 01:01:36 +0300

    creating new component for search part of page

---
+++ src/components/appSearch.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="search">
+    <app-top-content
+      :title="topContent.title"
+      :description="topContent.description"
+    />
+  </div>
+</template>
+
+<script>
+import AppTopContent from "@/components/appTopContent";
+export default {
+  name: "appSearch",
+  components: {AppTopContent},
+  data: () => ({
+    topContent: {
+      title: 'Заинтересовались?',
+      description: 'Укажите предполагаемые даты поездки, и мы покажем вам лучшие предложения гостиниц в седоне'
+    }
+  })
+}
+</script>
