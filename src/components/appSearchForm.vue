909a1493a9abd451c91f75170dc96894b4f0a3ac
Andrey <shock.work@mail.ru>
2021-01-04 01:28:02 +0300

    integrate this to form component

--- src/components/appSearchForm.vue
+++ src/components/appSearchForm.vue
@@ -5,14 +5,19 @@
     </div>
     <div class="form__map">
       <div class="form__body">
-
+        <app-form-text-input/>
       </div>
     </div>
   </form>
 </template>
 
 <script>
+import AppFormTextInput from "@/components/appFormTextInput";
+
 export default {
-  name: "appSearchForm"
+  name: "appSearchForm",
+  components: {
+    AppFormTextInput
+  }
 }
 </script>
