826761f8e43944fed09b71627995ea3a7f719e7e
Andrey <shock.work@mail.ru>
2021-01-03 19:35:17 +0300

    template for header component

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,13 +1,29 @@
 <template>
-  <header class="header"></header>
+  <header class="header">
+    <ul class="header__list">
+      <li class="header__item">
+        <router-link class="header__link" to="#">Информация</router-link>
+      </li>
+      <li class="header__item">
+        <router-link class="header__link" to="#">Фото и видео</router-link>
+      </li>
+      <li class="header__item">
+        <router-link class="header__link" to="/">
+          <img class="header__logo" src="../assets/img/logo.svg" alt="Logo">
+        </router-link>
+      </li>
+      <li class="header__item">
+        <router-link class="header__link" to="#">Карта штата</router-link>
+      </li>
+      <li class="header__item">
+        <router-link class="header__link" to="#">Гостиницы</router-link>
+      </li>
+    </ul>
+  </header>
 </template>
 
 <script>
 export default {
-name: "appHeader"
+  name: "appHeader"
 }
 </script>
-
-<style scoped>
-
-</style>
