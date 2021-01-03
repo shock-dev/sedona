38cbcb4efd6d2978e58334fee9970e7a88599031
Andrey <shock.work@mail.ru>
2021-01-03 20:19:05 +0300

    started files for advantages component

---
+++ src/components/appAdvantages.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="advantages">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "appAdvantages"
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="home">
     <app-intro/>
+    <app-advantages/>
   </div>
 </template>
 
 <script>
 import AppIntro from "@/components/appIntro";
+import AppAdvantages from "@/components/appAdvantages";
+
 export default {
   name: 'Home',
-  components: {AppIntro}
+  components: {
+    AppAdvantages,
+    AppIntro
+  }
 }
 </script>
