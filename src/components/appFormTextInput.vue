56f24798591b17afdf10ff4c0bb42cec3760fe72
Andrey <shock.work@mail.ru>
2021-01-04 01:40:11 +0300

    template for input

--- src/components/appFormTextInput.vue
+++ src/components/appFormTextInput.vue
@@ -1,6 +1,7 @@
 <template>
   <label class="form__label">
-
+    <span class="form__text">Дата заезда</span>
+    <input class="form__input" type="text">
   </label>
 </template>
 
@@ -9,7 +10,3 @@ export default {
   name: "appFormTextInput"
 }
 </script>
-
-<style scoped>
-
-</style>
