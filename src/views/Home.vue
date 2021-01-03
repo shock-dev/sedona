334c14b433f5f424aa5a10480d1dcb92b4220b8d
Andrey <shock.work@mail.ru>
2021-01-04 01:02:01 +0300

    integration this to a home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <app-intro/>
     <app-advantages/>
+    <app-search/>
   </div>
 </template>
 
 <script>
 import AppIntro from "@/components/appIntro";
 import AppAdvantages from "@/components/appAdvantages";
+import AppSearch from "@/components/appSearch";
 
 export default {
   name: 'Home',
   components: {
+    AppSearch,
     AppAdvantages,
     AppIntro
   }
