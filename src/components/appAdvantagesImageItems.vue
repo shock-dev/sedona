3cb3d1a5dc0fdc7371bca8d0bd8195a7a55bfca9
Andrey <shock.work@mail.ru>
2021-01-03 22:30:48 +0300

    template for advantages image items

---
+++ src/components/appAdvantagesImageItems.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="advantages__items image" :class="{ right: position !== 0 }">
+    <div class="advantages__item">
+      <div class="advantages__item-title">{{ title }}</div>
+      <div class="advantages__item-num">{{ getNumber }}</div>
+      <div class="advantages__item-text">{{ description }}</div>
+    </div>
+    <div class="advantages__img" :style="{ backgroundImage: 'url(' + imageUrl + ')' }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "appAdvantagesImageItems",
+  props: {
+    title: String,
+    description: String,
+    number: Number,
+    imageUrl: String,
+    position: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    getNumber() {
+      return `— №${this.number} —`
+    }
+  }
+}
+</script>
