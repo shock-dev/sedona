868f76c5343cb1a6a83411ba22c0efec7b321c47
Andrey <shock.work@mail.ru>
2021-01-05 00:35:06 +0300

    new component ~ Hotels page

---
+++ src/views/Hotels.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hotels">
+    <app-filter-panel/>
+  </div>
+</template>
+
+<script>
+import AppFilterPanel from "@/components/appFilterPanel";
+
+export default {
+  name: "Hotels",
+  components: {
+    AppFilterPanel
+  }
+}
+</script>
