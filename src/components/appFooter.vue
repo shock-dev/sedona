0644539473be7afec9783c1050a922804d4a1105
Andrey <shock.work@mail.ru>
2021-01-05 00:31:58 +0300

    footer component template

---
+++ src/components/appFooter.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="footer">
+    <ul class="footer__social social__list">
+      <li
+          v-for="({ title, imageUrl }, index) in social"
+          :key="index"
+          class="social__item"
+      >
+        <a class="social__link twitter" href="#">
+          <img :src="imageUrl" :alt="title">
+        </a>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "appFooter",
+  data: () => ({
+    social: [
+      {
+        title: 'twitter',
+        imageUrl: require('../assets/img/twitter.png')
+      },
+      {
+        title: 'facebook',
+        imageUrl: require('../assets/img/facebook.png')
+      },
+      {
+        title: 'youtube',
+        imageUrl: require('../assets/img/youtube.png')
+      }
+    ]
+  })
+}
+</script>
