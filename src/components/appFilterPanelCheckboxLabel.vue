c99a328603a3331fb2169bd4a3f6d6470c1411ae
Andrey <shock.work@mail.ru>
2021-01-05 18:06:41 +0300

    new component for checkbox label

---
+++ src/components/appFilterPanelCheckboxLabel.vue
@@ -0,0 +1,17 @@
+<template>
+  <label class="filter-panel__label">
+    <input class="filter-panel__checkbox-real" type="checkbox">
+    <span class="filter-panel__checkbox-fake"></span>
+    <span class="filter-panel__checkbox-text">Бассейн</span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "appFilterPanelCheckboxLabel"
+}
+</script>
+
+<style scoped>
+
+</style>
