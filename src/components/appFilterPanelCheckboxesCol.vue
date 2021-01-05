00dbf71dffb7ed47fa03dc701309a5f850c95adc
Andrey <shock.work@mail.ru>
2021-01-05 18:03:27 +0300

    new component for checkboxes col

---
+++ src/components/appFilterPanelCheckboxesCol.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="filter-panel__checkboxes-col">
+    <div class="filter-panel__title">Инфраструктура:</div>
+    <label class="filter-panel__label">
+      <input class="filter-panel__checkbox-real" type="checkbox">
+      <span class="filter-panel__checkbox-fake"></span>
+      <span class="filter-panel__checkbox-text">Бассейн</span>
+    </label>
+
+    <label class="filter-panel__label">
+      <input class="filter-panel__checkbox-real" type="checkbox">
+      <span class="filter-panel__checkbox-fake"></span>
+      <span class="filter-panel__checkbox-text">Бассейн</span>
+    </label>
+
+    <label class="filter-panel__label">
+      <input class="filter-panel__checkbox-real" type="checkbox">
+      <span class="filter-panel__checkbox-fake"></span>
+      <span class="filter-panel__checkbox-text">Бассейн</span>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+name: "appFilterPanelCheckboxesCol"
+}
+</script>
+
+<style scoped>
+
+</style>
