f529176c33c10fdc517158bfe8d45c103cb3b6e2
Andrey <shock.work@mail.ru>
2021-01-05 18:13:51 +0300

    import checkboxes col component to a parent

--- src/components/appFilterPanel.vue
+++ src/components/appFilterPanel.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="filter-panel">
-
+    <div class="filter-panel__checkboxes">
+      <app-filter-panel-checkboxes-col/>
+      <app-filter-panel-checkboxes-col/>
+    </div>
   </div>
 </template>
 
 <script>
+import AppFilterPanelCheckboxesCol from "@/components/appFilterPanelCheckboxesCol";
+
 export default {
-  name: "appFilterPanel"
+  name: "appFilterPanel",
+  components: {
+    AppFilterPanelCheckboxesCol
+  }
 }
 </script>
