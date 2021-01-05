49815444cba537af0d400ae1db16d10a56069ce1
Andrey <shock.work@mail.ru>
2021-01-05 18:46:39 +0300

    new component for price part

---
+++ src/components/appFilterPanelPrice.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="filter-panel__price">
+    <div class="filter-panel__title">Стоимость в сутки (Р):</div>
+    <div class="filter-panel__price-box">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "appFilterPanelPrice"
+}
+</script>
+
+<style scoped>
+
+</style>
