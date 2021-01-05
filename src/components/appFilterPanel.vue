1f299fdd4cbc5df670cf1c4194f8769f1ea7b72a
Andrey <shock.work@mail.ru>
2021-01-05 18:47:14 +0300

    import this to parent

--- src/components/appFilterPanel.vue
+++ src/components/appFilterPanel.vue
@@ -4,15 +4,18 @@
       <app-filter-panel-checkboxes-col/>
       <app-filter-panel-checkboxes-col/>
     </div>
+    <app-filter-panel-price/>
   </div>
 </template>
 
 <script>
 import AppFilterPanelCheckboxesCol from "@/components/appFilterPanelCheckboxesCol";
+import AppFilterPanelPrice from "@/components/appFilterPanelPrice";
 
 export default {
   name: "appFilterPanel",
   components: {
+    AppFilterPanelPrice,
     AppFilterPanelCheckboxesCol
   }
 }
