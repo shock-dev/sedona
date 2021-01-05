058ea6c8b46333657a808972ef3f62c2349ad4bb
Andrey <shock.work@mail.ru>
2021-01-06 00:08:13 +0300

    new component for list of card

---
+++ src/components/appHotels.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hotels">
+    <app-hotels-card/>
+    <app-hotels-card/>
+    <app-hotels-card/>
+  </div>
+</template>
+
+<script>
+import AppHotelsCard from "@/components/appHotelsCard";
+
+export default {
+  name: "appHotels",
+  components: {
+    AppHotelsCard
+  }
+}
+</script>
