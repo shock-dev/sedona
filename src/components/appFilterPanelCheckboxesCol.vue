119e0a528b94e340848e88fc00c8c4ed82970a0b
Andrey <shock.work@mail.ru>
2021-01-05 18:13:11 +0300

    import label component to checkboxes col component (no data yet)

--- src/components/appFilterPanelCheckboxesCol.vue
+++ src/components/appFilterPanelCheckboxesCol.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="filter-panel__checkboxes-col">
     <div class="filter-panel__title">Инфраструктура:</div>
-    <label class="filter-panel__label">
-      <input class="filter-panel__checkbox-real" type="checkbox">
-      <span class="filter-panel__checkbox-fake"></span>
-      <span class="filter-panel__checkbox-text">Бассейн</span>
-    </label>
-
-    <label class="filter-panel__label">
-      <input class="filter-panel__checkbox-real" type="checkbox">
-      <span class="filter-panel__checkbox-fake"></span>
-      <span class="filter-panel__checkbox-text">Бассейн</span>
-    </label>
-
-    <label class="filter-panel__label">
-      <input class="filter-panel__checkbox-real" type="checkbox">
-      <span class="filter-panel__checkbox-fake"></span>
-      <span class="filter-panel__checkbox-text">Бассейн</span>
-    </label>
+    <app-filter-panel-checkbox-label/>
+    <app-filter-panel-checkbox-label/>
+    <app-filter-panel-checkbox-label/>
   </div>
 </template>
 
 <script>
+import AppFilterPanelCheckboxLabel from "@/components/appFilterPanelCheckboxLabel";
+
 export default {
-name: "appFilterPanelCheckboxesCol"
+name: "appFilterPanelCheckboxesCol",
+  components: {AppFilterPanelCheckboxLabel}
 }
 </script>
 
