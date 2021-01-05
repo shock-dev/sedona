05539881ba040275fd88b61b6f9474e5ea7f700a
Andrey <shock.work@mail.ru>
2021-01-06 02:20:16 +0300

    module for hotels

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// importing modules
+import hotels from "@/store/modules/hotels";
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    hotels
   }
 })
