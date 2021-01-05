c053dee174a50de447654b22003db6f9a32ccbf1
Andrey <shock.work@mail.ru>
2021-01-05 18:57:08 +0300

    part that shows the price from and to

--- src/components/appFilterPanelPrice.vue
+++ src/components/appFilterPanelPrice.vue
@@ -2,7 +2,8 @@
   <div class="filter-panel__price">
     <div class="filter-panel__title">Стоимость в сутки (Р):</div>
     <div class="filter-panel__price-box">
-
+      <div class="filter-panel__price-box__item">от 0</div>
+      <div class="filter-panel__price-box__item">До 3000</div>
     </div>
   </div>
 </template>
