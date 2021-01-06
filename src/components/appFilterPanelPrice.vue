3d379e522e52c3e3029f3c0d9a7a8e87fdebf8d7
Andrey <shock.work@mail.ru>
2021-01-06 22:02:14 +0300

    init component

--- src/components/appFilterPanelPrice.vue
+++ src/components/appFilterPanelPrice.vue
@@ -6,14 +6,35 @@
       <div class="filter-panel__price-box__item">До 3000</div>
     </div>
     <div class="range-slider__wrapper">
-
+      <vue-slider
+          v-model="price"
+          :max="3000"
+          :tooltip="'none'"
+          :dotSize="20"
+          :height="2"
+          :railStyle="{ backgroundColor: '#a4a4a4' }"
+          :processStyle="{ backgroundColor: 'white' }"
+      >
+        <template v-slot:dot>
+          <div class="range-slider__dot"></div>
+        </template>
+      </vue-slider>
     </div>
     <button class="filter-panel__btn btn-reset">Показать</button>
   </div>
 </template>
 
 <script>
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
+
 export default {
-  name: "appFilterPanelPrice"
+  name: "appFilterPanelPrice",
+  components: {
+    VueSlider
+  },
+  data: () => ({
+    price: [500, 2500]
+  })
 }
 </script>
