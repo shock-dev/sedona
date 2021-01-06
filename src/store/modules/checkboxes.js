8fa4669299b19a0492e1862078465c62979c9348
Andrey <shock.work@mail.ru>
2021-01-06 18:56:25 +0300

    testing data for store

--- src/store/modules/checkboxes.js
+++ src/store/modules/checkboxes.js
@@ -1,1 +1,49 @@
-
+export default {
+    namespaced: true,
+    state: {
+        items: [
+            {
+                title: 'Инфраструктура:',
+                checkboxes: [
+                    {
+                        title: 'Бассейн',
+                        checked: true
+                    },
+                    {
+                        title: 'Парковка',
+                        checked: false
+                    },
+                    {
+                        title: 'Wi-Fi',
+                        checked: false
+                    }
+                ]
+            },
+            {
+                title: 'Тип жилья:',
+                checkboxes: [
+                    {
+                        title: 'Гостиница',
+                        checked: false
+                    },
+                    {
+                        title: 'Мотель',
+                        checked: false
+                    },
+                    {
+                        title: 'Апартаменты',
+                        checked: false
+                    }
+                ]
+            }
+        ]
+    },
+    mutations: {
+        setCheckboxChecked(state, payload) {
+            state.items[payload.index].checkboxes[payload.payload.index].checked = payload.payload.data
+        }
+    },
+    getters: {
+        items: ({ items }) => items
+    }
+}
