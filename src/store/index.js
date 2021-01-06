d49a57136b298e37e2a4750461fd9c6376ff346c
Andrey <shock.work@mail.ru>
2021-01-06 18:25:25 +0300

    new module ~ checkboxes

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import Vuex from 'vuex'
 
 // importing modules
 import hotels from "@/store/modules/hotels";
+import checkboxes from "@/store/modules/checkboxes";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    hotels
+    hotels,
+    checkboxes
   }
 })
