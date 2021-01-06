fae22677b7bd59a9f23cb2dbaf8f820076e09b78
Andrey <shock.work@mail.ru>
2021-01-06 18:08:56 +0300

    import component DatePicker

--- src/components/appFormTextInput.vue
+++ src/components/appFormTextInput.vue
@@ -13,12 +13,21 @@
 </template>
 
 <script>
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+
 export default {
   name: "appFormTextInput",
   props: {
     title: String,
     value: String
   },
+  components: {
+    DatePicker
+  },
+  data: () => ({
+    time: ''
+  }),
   methods: {
     sendValue(e) {
       this.$emit('sendValueToParent', e.target.value)
