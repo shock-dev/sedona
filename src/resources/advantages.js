7329a2b32407dec302484ca9211a825b4c4a8572
Andrey <shock.work@mail.ru>
2021-08-23 01:56:09 +0300

    prepare resources for home page

---
+++ src/resources/advantages.js
@@ -0,0 +1,35 @@
+export const whiteList = [
+  {
+    imageUrl: require('@/assets/img/home-icon.svg'),
+    title: 'Жильё',
+    description: 'Рекомендуем пожить в настоящем мотеле, всё как в кино!'
+  },
+  {
+    imageUrl: require('@/assets/img/hamburger-icon.svg'),
+    title: 'Еда',
+    description: 'Всегда заказывайте фирменный бургер, вы не разочаруетесь!'
+  },
+  {
+    imageUrl: require('@/assets/img/gift-icon.svg'),
+    title: 'Сувениры',
+    description: 'Не только китайского, но и местного производства!'
+  }
+]
+
+export const grayList = [
+  {
+    title: 'Небольшая площадь',
+    number: 3,
+    description: 'Все достопримечательности находятся очень близко'
+  },
+  {
+    title: 'Красивая дорога',
+    number: 4,
+    description: 'Ехать в Седону из Лас-Вегаса совсем не скучно!'
+  },
+  {
+    title: 'Мало туристов',
+    number: 5,
+    description: 'Большинство едет в гранд каньон и толпится там'
+  },
+]
